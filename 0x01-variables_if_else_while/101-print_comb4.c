a6f0fef6be25cbd9471354d6af72a5f08cf30004
Emperor <triplemtrack@yahoo.com>
2022-09-11 00:02:35 -0700

    Edited task

--- 0x01-variables_if_else_while/101-print_comb4.c
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -21,11 +21,11 @@ int main(void)
 						putchar(num1);
 						putchar(num2);
 						putchar(num3);
-						if (num1 != 55 || num2 != 56)
-						{
-							putchar(',');
-							putchar(' ');
-						}
+					if (num1 != 55 || num2 != 56)
+					{
+						putchar(',');
+						putchar(' ');
+					}
 					}
 				}
 			}
