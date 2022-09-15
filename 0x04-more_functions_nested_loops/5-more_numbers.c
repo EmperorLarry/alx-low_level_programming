f072a646551b556906957174a7feae14a4731f50
Emperor <triplemtrack@yahoo.com>
2022-09-15 15:15:16 -0700

    0x04-more_functions_nested_loops. You are so much sunshine in every square inch

---
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+/**
+ * more_numbers - prints 10 times the numbers, from 0 to 14
+ * followed by a new line
+ * Return: return void
+ */
+
+void more_numbers(void)
+{
+	int n, i;
+
+	for (i = 0; i < 10; i++)
+	{
+		for (n = 0; n < 15; n++)
+		{
+			if (n >= 10)
+				_putchar(n / 10 + '0');
+			_putchar(n % 10 + '0');
+		}
+		_putchar('\n');
+	}
+}
