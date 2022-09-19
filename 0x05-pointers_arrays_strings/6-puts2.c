5deb1e63a5b620fd7fc677d4610caa05d2b7d681
Emperor <triplemtrack@yahoo.com>
2022-09-19 14:34:08 -0700

    6. Half the lies they tell about me aren't true

---
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+/**
+ * puts2 - prints one char out of 2 of a string, followed by a new line
+ * @str: string to print the chars from
+ */
+
+void puts2(char *str)
+{
+	int lenght;
+	int k;
+
+	lenght = 0;
+	while (str[lenght] != '\0')
+	{
+		lenght++;
+	}
+	for (i = 0; k < lenght; k += 2)
+	{
+		_putchar(str[k]);
+	}
+	_putchar('\n');
+}
