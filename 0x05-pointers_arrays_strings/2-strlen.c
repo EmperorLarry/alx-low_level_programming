c2dec8042eda1f8a4ae66e0ba3a9ca982f2b18d2
Emperor <triplemtrack@yahoo.com>
2022-09-19 12:49:49 -0700

    2. This report, by its very length, defends itself against the risk of being read

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * _strlen - a function that returns the length of a string
+ * @s: the string to be checked
+ *
+ * Return: the length of the string
+ */
+
+int _strlen(char *s)
+{
+
+	return (strlen(s));
+
+}
