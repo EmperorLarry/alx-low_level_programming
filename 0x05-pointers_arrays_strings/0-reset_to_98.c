4c729739b6ef04f335000d5d28cede13e29f706f
Emperor <triplemtrack@yahoo.com>
2022-09-19 11:56:42 -0700

    0. 98 Battery street

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+ * reset_to_98 - assesses the address of a variable and changes its value.
+ *
+ * @a: is the pointer to the variable to update
+ */
+
+void reset_to_98(int *a)
+{
+
+	*a = 98;
+
+}
---
+++ 0x05-pointers_arrays_strings/main.h
@@ -0,0 +1,14 @@
+#ifndef MAIN_H
+#define MAIN_H
+ /**
+  * Name -  main.h header files
+  * This header files contains functions declarations
+  */
+
+ int _putchar(char c)
+
+
+
+
+
+#endif
