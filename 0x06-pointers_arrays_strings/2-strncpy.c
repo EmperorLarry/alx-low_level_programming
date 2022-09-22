5be4bd96ceddf405ae77dd6cc9c6e5de97af09f0
Emperor <triplemtrack@yahoo.com>
2022-09-22 14:38:00 -0700

    function that concatenates, copy and compare two strings.

---
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -0,0 +1,26 @@
+#include "main.h"
+
+/**
+ * _strncpy - a function that copies a string, and its terminating null byte,
+ * using at most an inputted number of bytes.
+ * @dest: buffer storing the string copy
+ * @src:the source string
+ * @n: max number of bytes to copy
+ * Return: returns
+ */
+
+char *_strncpy(char *dest, char *src, int n)
+{
+	int z;
+
+	for (z = 0; z < n && src[z] != '\0'; z++)
+	{
+		dest[z] = src[z];
+	}
+	for (z < n; z++)
+	{
+		dest[z] = '\0';
+	}
+	return (dest);
+
+}
